0ce54e7e898dc01ed7cadcb481918109e6f50681
eespitia16 <eespitiapicon@gmail.com>
2018-09-03 22:20:35 -0500

    cambio 10

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ window.addEventListener('scroll', () =>{
     if(window.scrollY>0) navBar.classList.add('is-down')
     else navBar.classList.remove('is-down')
 })
+
+var btnMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+btnMenu.addEventListener('click', function(){
+  nav.classList.toggle('mostrar');
+});
